58410d6ad0fed1758453523170d15db282df6db5
Ashley <vickyusq@gmail.com>
2018-10-01 10:09:24 +0800

    add classnotes

--- 2018-09-19/9_19 classnotes.R
+++ 2018-09-19/9_19 classnotes.R
@@ -3,8 +3,8 @@
 - save
   1. Set with encoding\utf8 確定存的東西在別台電腦打開不會變成亂碼
 - Github
-  1. repositories 專案
-  2. commit : R跟Github做連接 
+  1. add repositories 專案
+  2. publish repository : R跟Github做連接 
             追蹤版本(加減任何一行可以做還原)
 # R Basics
 
